3d3a90294108355cafc84caca978bdba58e70dc5
Athish-GK <160147619+Athish-GK@users.noreply.github.com>
2025-04-23 20:01:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const humidity = document.querySelector(".humidity-info");
 const wind = document.querySelector(".wind-info");
 const clouds = document.querySelector(".clouds-info");
 const map = document.querySelector(".location-info");
+const time = document.querySelector(".time-info");
 
 let getWeather = async (loc) => {
     try {
@@ -61,6 +62,24 @@ search.addEventListener("click", () => {
             wind.textContent = `${response.wind.speed} m/s`;
             clouds.textContent = `${response.clouds.all}%`;
             map.textContent = `${response.name}, ${response.sys.country}`;
+            let localTimestamp = (response.dt + response.timezone) * 1000;
+            let localTime = new Date(localTimestamp);
+
+            let dayDate = localTime.toLocaleDateString('en-US', {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            });
+            let timeString = localTime.toLocaleTimeString('en-US', {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: true,
+                timeZone: 'UTC'
+            });
+
+            time.textContent = `${dayDate} | ${timeString}`;
+
 
             document.querySelector('.ans').classList.remove('hidden');
             document.querySelector('.error').classList.add('hidden');
